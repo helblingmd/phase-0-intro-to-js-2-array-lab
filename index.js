84b498cb73e6840f4941cb4481a8fe418563be42
Mark Helbling <mdhelbling@gmail.com>
2022-12-14 11:14:00 -0600

    commit tree 3099e93aa130f2057fe6a519f597947599c4e992

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+
+
+const destructivelyAppendCat = cats.push("Ralph");
+
+cats.length = 0;
+  
+cats.push("Milo", "Otis", "Garfield");
